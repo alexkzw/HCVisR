cb2c8f1100708f1f14735aecace7eed2a16674d9
alexkzw <alex.koh@stats.govt.nz>
2024-09-15 11:23:59 +1200

    implement main app UI

---
+++ R/app_ui.R
@@ -0,0 +1,19 @@
+# R/app_ui.R
+app_ui <- function(request) {
+    fluidPage(
+        titlePanel("Time Series Generation and H x C Visualisation"),
+
+        sidebarLayout(
+            sidebarPanel(
+                mod_time_series_ui("timeseries"),
+                mod_file_upload_ui("file_upload"),
+                mod_time_series_selection_ui("available_series"),
+                selectInput("embedding_dimension", "Select Embedding Dimension:",
+                            choices = 3:6, selected = 3)
+            ),
+            mainPanel(
+                mod_plot_ui("plot")
+            )
+        )
+    )
+}
