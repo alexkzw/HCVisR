d0427da1ff26398ebd3acd67fb5ba2b419d4d187
Alex.Koh <alexkzw@hotmail.com>
2024-09-15 13:28:50 +1200

    tidy code

--- inst/app.R
+++ inst/app.R
@@ -1,18 +1,12 @@
 library(shiny)
-library(shiny)
 library(ggplot2)
 library(statcomp)
 library(StatOrdPattHxC)
 library(tuneR)
 
-# During development, manually source the scripts from the R/ folder.
-source("R/app_ui.R")
-source("R/app_server.R")
-source("R/time_series_classes.R")
-source("R/mod_time_series.R")
-source("R/mod_file_upload.R")
-source("R/mod_time_series_selection.R")
-source("R/mod_plot.R")
+ui <- HCVisR::app_ui()
+server <- function(input, output, session) {
+    HCVisR::app_server(input, output, session)
+}
 
-# Once you've sourced all your files, you can run the Shiny app.
-shinyApp(ui = app_ui, server = app_server)
+shiny::shinyApp(ui = ui, server = server)
