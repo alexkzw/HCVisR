495ecc7e9a5178422fa82a80da211f33ab031565
Alex.Koh <alexkzw@hotmail.com>
2024-10-04 14:29:51 +1300

    incorporate unit tests to file upload UI

--- R/mod_file_upload.R
+++ R/mod_file_upload.R
@@ -31,11 +31,27 @@ mod_file_upload_server <- function(id, available_series) {
                 return()
             }
             req(input$file)
+
             tryCatch({
-                file_data <- utils::read.csv(input$file$datapath, header = TRUE)
-                time_series_column <- file_data[[1]]  # Assume the first column contains the time series
+                file_data <- utils::read.csv(input$file$datapath, header = FALSE)
+
+                # Check if the file has more than one column
+                if (ncol(file_data) != 1){
+                    showNotification("The file must contain only one column with time series data.", type = "error")
+                    return()
+                }
+                # Assume the first column contains the time series
+                time_series_column <- file_data[[1]]
+
+                # Ensure the time series data is numeric
+                if (!is.numeric(time_series_column)) {
+                    showNotification("The time series data must be numeric.", type = "error")
+                    return()
+                }
+                # Create a new time series object and add it to the available series
                 ts_obj <- new_timeseries(series = time_series_column, model = input$file$name)  # Use filename as model name
                 available_series(c(available_series(), list(ts_obj)))  # Add to available series
+
             }, error = function(e) {
                 showNotification("Error in processing the file. Ensure it's a valid CSV with numeric data in the first column.", type = "error")
             })
