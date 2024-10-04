f5aa7200e9f44d8740d0f081f67da4da20f21a1d
Alex.Koh <alexkzw@hotmail.com>
2024-10-04 14:31:34 +1300

    incorporate unit tests for multiple embedding dimensions when plotting in HxC plane

--- tests/testthat/test_mod_plot.R
+++ tests/testthat/test_mod_plot.R
@@ -1,3 +1,23 @@
+test_that("H x C plot is generated for multiple embedding dimensions", {
+    shiny::testServer(
+        mod_plot_server,
+        args = list(
+            selected_series_data = reactiveVal(list(series = rnorm(100), model = "WN")),
+            embedding_dimension = reactiveVal(3)
+        ),
+        {
+            session$flushReact()
+            expect_error(output$plot_hc, NA)
+
+            for (emb_dim in 3:6) {
+                embedding_dimension(emb_dim)
+                session$flushReact()
+                expect_error(output$plot_hc, NA)
+            }
+        }
+    )
+})
+
 test_that("Plots are generated correctly", {
     shiny::testServer(
         mod_plot_server,
@@ -8,10 +28,8 @@ test_that("Plots are generated correctly", {
         {
             # Trigger reactive updates
             session$flushReact()
-
             # Test that plot_ts is created without errors
             expect_error(output$plot_ts, NA)
-
             # Test that plot_hc is created without errors
             expect_error(output$plot_hc, NA)
         }
